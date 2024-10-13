424d8f578c220961eb971ad87e66c2060244aae8
MMbats <petermbatia657@gmail.com>
2024-10-13 17:04:35 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Grab the element by its id
+    const input = document.getElementById('button');
+    
+    // Create a named function for the alert message
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    // Add the event listener for 'click' event
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to activate the event listener
+  addingEventListener();
+  
